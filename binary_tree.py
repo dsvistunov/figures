dc239b71216367f47d1113c115ff4316bff86239
dsvistunov <d.svistuno0v1991@gmail.com>
2018-10-02 11:42:33 +0300

    added delete method

--- binary_tree.py
+++ binary_tree.py
@@ -55,8 +55,83 @@ class BinaryTree:
         else:
             return self._find(key, current)
 
+    def delete(self, key):
+        if self.size > 1:
+            node = self._find(key, self.root)
+            if node:
+                self.remove(node)
+                self.size -= 1
+            else:
+                raise KeyError('Key not in tree')
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size -= 1
+        else:
+            raise KeyError('Key not in tree')
+
+    def findSuccessor(self):
+        succ = None
+        if self.right:
+            succ = self.right.findMin()
+        else:
+            if self.parent:
+                if self.parent and self.parent.left == self:
+                    succ = self.parent
+                else:
+                    self.parent.right = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.right = self
+        return succ
+
+    def findMin(self):
+        current = self
+        while current.left:
+            current = current.left
+        return current
+
+    def remove(self, current):
+        if current.left:
+            if current == current.parent.left:
+                current.parent.left = None
+            else:
+                current.parent.right = None
+        elif current.left and current.right:
+            succ = current.findSuccessor()
+            succ.spliceOut()
+            current.key = succ.key
+            current.payload = succ.payload
+        else:
+            if current.left:
+                if current.parent and current.parent.left == current:
+                    current.left.parent = current.parent
+                    current.parent.left = current.left
+                elif current.parent and current.parent.right == current:
+                    current.left.parent = current.parent
+                    current.parent.right = current.left
+                else:
+                    current.key = current.left.key
+                    current.payload = current.left.payload
+                    current.left = current.left.left
+                    current.right = current.left.right
+            else:
+                if current.parent and current.parent.left == current:
+                    current.left.parent = current.parent
+                    current.parent.left = current.right
+                elif current.parent and current.parent.right == current:
+                    current.left.parent = current.parent
+                    current.parent.right = current.right
+                else:
+                    current.key = current.right.key
+                    current.payload = current.right.payload
+                    current.left = current.right.left
+                    current.right = current.right.right
+
+
+
 
 if __name__ == '__main__':
     tree = BinaryTree()
     tree.add('key', 'value')
+    print(tree.find('key'))
+    tree.delete('key')
     print(tree.find('key'))
